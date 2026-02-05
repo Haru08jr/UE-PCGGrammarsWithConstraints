6a920596f40179305894d050ecd6225cdffc7031
Haru08jr <jreutercn@gmail.com>
2026-02-05 12:15:36 +0100

    Initial commit

---
+++ Source/PCGGrammarsWithConstraints/Private/Nodes/PCGConstrainGrammar.cpp
@@ -0,0 +1,144 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Nodes/PCGConstrainGrammar.h"
+
+#include "Data/PCGBasePointData.h"
+#include "Data/PCGSplineData.h"
+
+TArray<FPCGPinProperties> UPCGConstrainGrammarSettings::InputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+
+	if (bShapeAsInput)
+	{
+		FPCGPinProperties& InShapePin = PinProperties.Emplace_GetRef(PCGPinConstants::DefaultInputLabel, SubdivisionType == Spline ? EPCGDataType::PolyLine : EPCGDataType::Point,
+		                                                             false, true, FText::FromString("The Spline on which the Grammar will be generated. Should match the In pin of Subdivide Spline."));
+		InShapePin.SetRequiredPin();
+	}
+
+	if (bConstraintsAsInput)
+	{
+		FPCGPinProperties& InConstraintsPin = PinProperties.Emplace_GetRef(PCGConstrainGrammar::Constants::ConstraintsPinLabel, EPCGDataType::Point,
+		                                                                   false, true, FText::FromString("The Constraints to apply to the Grammar."));
+		InConstraintsPin.SetRequiredPin();
+	}
+
+	if (bModuleInfoAsInput)
+	{
+		FPCGPinProperties& ModuleInfoPin = PinProperties.Emplace_GetRef(PCGSubdivisionBase::Constants::ModulesInfoPinLabel, EPCGDataType::Param);
+		ModuleInfoPin.SetRequiredPin();
+	}
+
+	return PinProperties;
+}
+
+TArray<FPCGPinProperties> UPCGConstrainGrammarSettings::OutputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PinProperties.Emplace(TEXT("OutGrammar"), EPCGDataType::Param);
+	return PinProperties;
+}
+
+FPCGElementPtr UPCGConstrainGrammarSettings::CreateElement() const
+{
+	return MakeShared<FPCGConstrainGrammarElement>();
+}
+
+bool FPCGConstrainGrammarElement::ExecuteInternal(FPCGContext* InContext) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGSubdivideSplineElement::Execute);
+
+	const UPCGConstrainGrammarSettings* Settings = InContext->GetInputSettings<UPCGConstrainGrammarSettings>();
+	check(Settings);
+
+	TArray<FPCGTaggedData>& Outputs = InContext->OutputData.TaggedData;
+	
+	const UPCGParamData* ModuleInfoParamData = nullptr;
+	const FModuleInfoMap ModulesInfo = GetModulesInfo(InContext, Settings, ModuleInfoParamData);
+
+	if (!Settings->bShapeAsInput)
+	{
+		auto ConstrainedSting = ConstrainGrammar(Settings->GrammarSelection.GrammarString, Settings->Length, ModulesInfo, Settings->Constraints);
+		
+		// TODO output, I don't want to think about this now
+	}
+	else
+	{
+		if (Settings->SubdivisionType == Spline)
+		{
+			// TODO stuff
+		}
+		else if (Settings->SubdivisionType == Segment)
+		{
+			// TODO stuff
+		}
+		else
+		{
+			// TODO error
+		}
+	}
+
+	return false;
+}
+
+FString FPCGConstrainGrammarElement::ConstrainGrammar(const FString& GrammarString, float Length, const FModuleInfoMap& Modules, const TArray<FPCGGrammarConstraint> Constraints) const
+{
+	// TODO use implementation
+	return GrammarString;
+}
+
+float FPCGConstrainGrammarElement::GetShapeLength(const FPCGContext* InContext) const
+{
+	const UPCGConstrainGrammarSettings* Settings = InContext->GetInputSettings<UPCGConstrainGrammarSettings>();
+
+	if (!Settings->bShapeAsInput)
+		return Settings->Length;
+
+	const TArray<FPCGTaggedData> InputShapes = InContext->InputData.GetInputsByPin(PCGPinConstants::DefaultInputLabel);
+	if (!Settings->SubdivisionType == Spline)
+	{
+		const UPCGPolyLineData* InputSplineData = Cast<const UPCGPolyLineData>(InputShapes[0].Data);
+		return InputSplineData->GetLength();
+	}
+
+	if (!Settings->SubdivisionType == Segment)
+	{
+		const UPCGBasePointData* InputSegmentData = Cast<const UPCGBasePointData>(InputShapes[0].Data);
+		auto Bounds = InputSegmentData->GetBoundsMax(0) - InputSegmentData->GetBoundsMin(0);
+
+		switch (Settings->SubdivisionAxis)
+		{
+		case EPCGSplitAxis::X: return Bounds.X;
+		case EPCGSplitAxis::Y: return Bounds.Y;
+		case EPCGSplitAxis::Z: return Bounds.Z;
+		}
+	}
+
+	// TODO error message
+	return 0.0;
+}
+
+PCGSubdivisionBase::FModuleInfoMap FPCGConstrainGrammarElement::GetModulesInfo(FPCGContext* InContext, const UPCGConstrainGrammarSettings* InSettings,
+                                                                               const UPCGParamData*& OutModuleInfoParamData) const
+{
+	if (InSettings->bModuleInfoAsInput)
+	{
+		return GetModulesInfoMap(InContext, InSettings->ModulesInfoAttributeNames, OutModuleInfoParamData);
+	}
+	else
+	{
+		return GetModulesInfoMap(InContext, InSettings->ModulesInfo, OutModuleInfoParamData);
+	}
+}
+
+FString FPCGConstrainGrammarElement::GetGrammarString(FPCGContext* InContext, const UPCGConstrainGrammarSettings* InSettings) const
+{
+	if (!InSettings->GrammarSelection.bGrammarAsAttribute)
+	{
+		return InSettings->GrammarSelection.GrammarString;
+	}
+	
+	//TODO other stuff
+	return "";
+}
---
+++ Source/PCGGrammarsWithConstraints/Public/Nodes/PCGConstrainGrammar.h
@@ -0,0 +1,118 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGSettings.h"
+#include "Elements/PCGSplitPoints.h"
+#include "Elements/Grammar/PCGSubdivisionBase.h"
+
+#include "PCGConstrainGrammar.generated.h"
+
+USTRUCT(BlueprintType)
+struct FPCGGrammarConstraint
+{
+	GENERATED_BODY()
+	/** Symbol in the grammar. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "")
+	FName Symbol = NAME_None;
+	
+	/** Position along the generation shape. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "")
+	double Position = 100.0;
+	
+	/** Position along the generation shape. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "")
+	bool bHasWidth = false;
+	
+	/** Position along the generation shape. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "", meta=(EditCondition = "bHasWidth"))
+	double Width = 100.0;
+	
+};
+
+UENUM()
+enum SubdivisionType
+{
+	Spline,
+	Segment
+};
+
+namespace PCGConstrainGrammar::Constants
+{
+	const FName ConstraintsPinLabel = TEXT("Constraints");
+}
+
+UCLASS(BlueprintType, ClassGroup = (Procedural))
+class PCGGRAMMARSWITHCONSTRAINTS_API UPCGConstrainGrammarSettings : public UPCGSettings
+{
+	GENERATED_BODY()
+	
+public:
+	//~Begin UPCGSettings interface
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override { return FName(TEXT("ConstrainGrammar")); }
+	virtual FText GetDefaultNodeTitle() const override { return NSLOCTEXT("PCGConstrainGrammarElement", "NodeTitle", "Constrain Grammar"); }
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Spatial; }
+#endif // WITH_EDITOR
+	
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings interface
+	
+public:	
+	/** If true, takes in a shape input along which the grammar will be generated. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|InputShape", meta = (PCG_Overridable))
+	bool bShapeAsInput = false;
+	
+	/** The type of subdivision node that will be used. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|InputShape", meta = (EditCondition = "bShapeAsInput", EditConditionHides, PCG_Overridable))
+	TEnumAsByte<SubdivisionType> SubdivisionType = Spline;
+	
+	/** Subdivision direction in point local space. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|InputShape", meta = (EditCondition = "bShapeAsInput && (SubdivisionType == SubdivisionType::Segment)", EditConditionHides, PCG_Overridable))
+	EPCGSplitAxis SubdivisionAxis = EPCGSplitAxis::X;
+	
+	/** The length of the shape along which the grammar will be generated. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|InputShape", meta = (EditCondition = "!bShapeAsInput", EditConditionHides, PCG_Overridable))
+	float Length = 0.0;
+	
+	/** Set it to true to pass the info as attribute set. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Modules")
+	bool bModuleInfoAsInput = false;
+
+	/** Fixed array of modules used for the subdivision. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Modules", meta = (EditCondition = "!bModuleInfoAsInput", EditConditionHides, DisplayAfter = bModuleInfoAsInput))
+	TArray<FPCGSubdivisionSubmodule> ModulesInfo;
+
+	/** Fixed array of modules used for the subdivision. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Modules", meta = (EditCondition = "bModuleInfoAsInput", EditConditionHides, DisplayAfter = bModuleInfoAsInput, DisplayName = "Attribute Names for Module Info"))
+	FPCGSubdivisionModuleAttributeNames ModulesInfoAttributeNames;
+	
+	/** If true, takes in the constraint positions as points. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Constraints", meta = (EditCondition = "bShapeAsInput", EditConditionHides, PCG_Overridable))
+	bool bConstraintsAsInput = false;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Constraints", meta = (EditCondition = "!bShapeAsInput || !bConstraintsAsInput", EditConditionHides, PCG_Overridable))
+	TArray<FPCGGrammarConstraint> Constraints;
+	
+	/** An encoded string that represents how to apply a set of rules to a series of defined modules. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings|Grammar", meta = (ShowOnlyInnerProperties, PCG_Overridable))
+	FPCGGrammarSelection GrammarSelection;
+	
+};
+
+class FPCGConstrainGrammarElement : public FPCGSubdivisionBaseElement
+{
+protected:
+	virtual bool ExecuteInternal(FPCGContext* InContext) const override;
+	
+	FString ConstrainGrammar(const FString& GrammarString, float Length, const FModuleInfoMap& Modules, const TArray<FPCGGrammarConstraint> Constraints) const;
+	
+	float GetShapeLength(const FPCGContext* InContext) const;
+	
+	PCGSubdivisionBase::FModuleInfoMap GetModulesInfo(FPCGContext* InContext, const UPCGConstrainGrammarSettings* InSettings, const UPCGParamData*& OutModuleInfoParamData) const;
+	FString GetGrammarString(FPCGContext* InContext, const UPCGConstrainGrammarSettings* InSettings) const;
+};
