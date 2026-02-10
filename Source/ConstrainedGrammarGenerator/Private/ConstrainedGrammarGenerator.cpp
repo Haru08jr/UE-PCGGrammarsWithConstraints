7b21b70b4f6a77d0aa552be2cf61f32e3e32b776
Haru08jr <jreutercn@gmail.com>
2026-02-10 14:55:30 +0100

    Removed submodule and added unreal module

---
+++ Source/ConstrainedGrammarGenerator/Private/ConstrainedGrammarGenerator.cpp
@@ -0,0 +1,17 @@
+﻿#include "ConstrainedGrammarGenerator.h"
+
+#define LOCTEXT_NAMESPACE "FConstrainedGrammarGeneratorModule"
+
+void FConstrainedGrammarGeneratorModule::StartupModule()
+{
+    
+}
+
+void FConstrainedGrammarGeneratorModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FConstrainedGrammarGeneratorModule, ConstrainedGrammarGenerator)
